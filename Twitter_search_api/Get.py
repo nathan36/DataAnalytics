fe82fc6cfacf83200e3cebbc78a39e7ce7a4fd95
nathanLee <nathanli3636@gmail.com>
2016-07-08 05:47:57 -0700

    update

--- Twitter_search_api/Get.py
+++ Twitter_search_api/Get.py
@@ -2,6 +2,7 @@ from tweepy import API
 from tweepy import AppAuthHandler
 from tweepy import TweepError
 from GetOldTweets import manager
+import time
 
 access_token = "707079396922773504-Vr0Com9qRPepTEudQhq03Lhe8zYAXwv"
 access_token_secret = "ycmLrMJBehXUa05aha3ETjDyvhhHeKrVJ2DVpAK9CEkyU"
@@ -12,13 +13,15 @@ auth = AppAuthHandler(consumer_key, consumer_secret)
 api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 # set search key word, data range and maximum number of tweets for capturing
+# Note: twitter search api unstable, for first time search, since unit period
+# recommend to be less than a month
 
-tweetCriteria = manager.TweetCriteria().setQuerySearch('hyperwallet')\
-    .setSince("2016-01-01").setUntil("2016-07-07").setMaxTweets(0)
+tweetCriteria = manager.TweetCriteria().setQuerySearch('hootsuite')\
+    .setSince("2016-06-01").setUntil("2016-07-07").setMaxTweets(0)
 
 data = manager.TweetManager.getTweets(tweetCriteria)
 
-tweet = '../Data/twitter_data.txt'
+tweet = '../Data/' + str(time.time()) + 'twitter_data.txt'
 with open(tweet, 'w') as outfile:
     for line in data:
         try:
